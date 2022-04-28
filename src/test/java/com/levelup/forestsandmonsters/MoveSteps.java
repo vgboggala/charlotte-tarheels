368af436472be45d27f315fd7fefbabc82a22b97
root <venu.boggala@live.com>
2022-04-28 13:57:28 -0400

    renamed

--- src/test/java/com/levelup/forestsandmonsters/MoveSteps.java
+++ src/test/java/com/levelup/forestsandmonsters/MoveSteps.java
@@ -9,7 +9,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import java.awt.Point;
 
-public class MoveTest {
+public class MoveSteps {
     GameController gc;    
     Point position;
     
